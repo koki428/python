3c3f468a5a69491781d18b50e2f963b775f2bda8
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2019-04-18 22:18:43 +0900

    init.pyを追加

---
+++ init.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import read
+import config as c
+import sys
+import os
+
+try:
+    caseid
+except NameError:
+    print("input caseid id (3 digit)")
+    caseid = 0
+    caseid = input()
+    caseid = "d"+caseid.zfill(3)
+
+dir="../run/"+caseid+"/data/"
+casedir="../figs/"+caseid
+os.makedirs(casedir,exist_ok=True)
+
+read.read_init(dir,"3d")
+for key in c.p:
+    exec('%s = %s%s%s' % (key, 'c.p["',key,'"]'))
+
+try:
+    n0
+except NameError:
+    n0 = 0
+if  n0 > c.p["nd"]:
+    n0 = c.p["nd"]
+
