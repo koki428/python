9344fbd857b7cc405d90437f790e9bfbdbbe854f
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2019-04-19 23:22:20 +0900

    mov.pyとest.pyを追加した。これから育てていく

---
+++ est.py
@@ -0,0 +1,233 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import read
+import config as c
+import sys
+
+try:
+    caseid
+except NameError:
+    print("input caseid id (3 digit)")
+    caseid = 0
+    caseid = input()
+    caseid = "d"+caseid.zfill(3)
+
+dir="../run/"+caseid+"/data/"
+
+read.read_init(dir,"3d")
+for key in c.p:
+    exec('%s = %s%s%s' % (key, 'c.p["',key,'"]'))
+
+try:
+    n0
+except NameError:
+    n0 = 0
+if  n0 > c.p["nd"]:
+    n0 = c.p["nd"]
+
+print("Maximum time step= ",nd," time ="\
+      ,dtout*float(nd)/3600./24.," [day]")
+
+plt.close('all')
+
+vxrmst = np.zeros((ix,nd-n0+1))
+vyrmst = np.zeros((ix,nd-n0+1))
+vzrmst = np.zeros((ix,nd-n0+1))
+
+bxrmst = np.zeros((ix,nd-n0+1))
+byrmst = np.zeros((ix,nd-n0+1))
+bzrmst = np.zeros((ix,nd-n0+1))
+
+rormst = np.zeros((ix,nd-n0+1))
+sermst = np.zeros((ix,nd-n0+1))
+prrmst = np.zeros((ix,nd-n0+1))
+termst = np.zeros((ix,nd-n0+1))
+
+romt = np.zeros((ix,nd-n0+1))
+semt = np.zeros((ix,nd-n0+1))
+prmt = np.zeros((ix,nd-n0+1))
+temt = np.zeros((ix,nd-n0+1))
+
+fet = np.zeros((ix,nd-n0+1))
+fdt = np.zeros((ix,nd-n0+1))
+fkt = np.zeros((ix,nd-n0+1))
+frt = np.zeros((ix,nd-n0+1))
+ftt = np.zeros((ix,nd-n0+1))
+
+for n in range(n0,nd+1):
+#for n in range(0,1):
+    print(n)
+    ##############################
+    # read time
+    f = open(dir+"time/t.dac."+'{0:08d}'.format(n),"rb")
+    t = np.fromfile(f,endian+'d',1)
+    f.close()    
+    t = np.reshape(t,(1),order="F")
+    
+    ##############################
+    # read value
+    f = open(dir+"remap/vla.dac."+'{0:08d}'.format(n),"rb")
+    vl0 = np.fromfile(f,c.p["endian"]+'f',m2da*ix*jx)
+    f.close()
+
+    vl = np.reshape(vl0,(ix,jx,m2da),order="F")
+
+    vc = {"a":0}
+    for m in range(c.p["m2da"]):
+        vc[c.p["cl"][m]] = vl[:,:,m]
+        
+    ##############################    
+    fsun = 6.3e10
+    fe = np.average(vc["fe"],axis=1)
+    fd = np.average(vc["fd"],axis=1)
+    fk = np.average(vc["fk"],axis=1)
+    fr = np.average(vc["fr"],axis=1)
+
+    xs = c.p["rsun"] - 2.e8
+    ds = 2.e7
+    sr = 0.5e0*(1.e0 + np.tanh((c.p["x"]-xs)/ds))
+    SR, sry = np.meshgrid(sr,y,indexing="ij")
+    
+    ff = fd*sr + fe*(1.e0-sr)
+    ft = ff + fk + fr
+
+    vxrmst[:,n-n0] = np.sqrt(np.average(vc["vxrms"]**2,axis=1))
+    vyrmst[:,n-n0] = np.sqrt(np.average(vc["vyrms"]**2,axis=1))
+    vzrmst[:,n-n0] = np.sqrt(np.average(vc["vzrms"]**2,axis=1))
+
+    bxrmst[:,n-n0] = np.sqrt(np.average(vc["bxrms"]**2,axis=1))
+    byrmst[:,n-n0] = np.sqrt(np.average(vc["byrms"]**2,axis=1))
+    bzrmst[:,n-n0] = np.sqrt(np.average(vc["bzrms"]**2,axis=1))
+
+    rormst[:,n-n0] = np.sqrt(np.average(vc["rorms"]**2,axis=1))
+    sermst[:,n-n0] = np.sqrt(np.average(vc["serms"]**2,axis=1))
+    prrmst[:,n-n0] = np.sqrt(np.average(vc["prrms"]**2,axis=1))
+    termst[:,n-n0] = np.sqrt(np.average(vc["terms"]**2,axis=1))
+
+    romt[:,n-n0] = np.sqrt(np.average(vc["rom"]**2,axis=1))
+    semt[:,n-n0] = np.sqrt(np.average(vc["sem"]**2,axis=1))
+    prmt[:,n-n0] = np.sqrt(np.average(vc["prm"]**2,axis=1))
+    temt[:,n-n0] = np.sqrt(np.average(vc["tem"]**2,axis=1))
+
+    fet[:,n-n0] = fe
+    fdt[:,n-n0] = fd
+    fkt[:,n-n0] = fk
+    frt[:,n-n0] = fr
+    ftt[:,n-n0] = ft
+
+    fontsize = 12
+    fmax = 3.0
+    fmin = -2.0
+
+    plt.rcParams["font.size"] = 15
+    fig1 = plt.figure(num=1,figsize=(12,5))
+    ax1 = fig1.add_subplot(121)
+    ax2 = fig1.add_subplot(122)
+    ax1.plot(c.p["xr"],ff/fsun,color="red",label="$F_\mathrm{e}$")
+    ax1.plot(c.p["xr"],fk/fsun,color="green",label="$F_\mathrm{k}$")
+    ax1.plot(c.p["xr"],fr/fsun,color="blue",label="$F_\mathrm{r}$")
+    ax1.plot(c.p["xr"],ft/fsun,color="black",label="$F_\mathrm{t}$")
+    ax1.set_xlim(c.p["xmin"]/c.p["rsun"],c.p["xmax"]/c.p["rsun"])
+    ax1.set_ylim(fmin,fmax)
+    ax1.set_xlabel("$x/R_{\odot}$")
+    ax1.set_ylabel("$F/F_{\odot}$")
+    ax1.set_title("Full convection zone")
+    ax1.legend(loc='upper left',prop={'size': 10})
+    ax1.annotate(s="t="+"{:.2f}".format(t[0]/3600./24.)+" [day]"\
+                     ,xy=[0.01,0.01],xycoords="figure fraction",fontsize=18)
+    
+    xtmp = 0.5*(x[ix//2] + x[ix//2-1])
+    ax1.vlines(xtmp/rsun,-2,3)
+
+    #####################
+    ax2.plot(c.p["xn"],ff/fsun,color="red")
+    ax2.plot(c.p["xn"],fk/fsun,color="green")
+    ax2.plot(c.p["xn"],fr/fsun,color="blue")
+    ax2.plot(c.p["xn"],ft/fsun,color="black")
+    ax2.set_xlim(-20,1)
+    ax2.set_ylim(fmin,fmax)
+    ax2.set_xlabel("$x - R_{\odot} \ [\mathrm{Mm}]$")
+    ax2.set_ylabel("$F/F_{\odot}$")
+    ax2.set_title("Around photosphere")
+
+    if n == n0:
+        plt.tight_layout()
+    plt.pause(0.001)
+    #plt.draw()
+    
+    if n != nd:
+        plt.clf() # clear figure
+
+    # loop end
+    ###############################################################################
+    ###############################################################################
+    ###############################################################################
+
+vxrms = np.sqrt(np.average(vxrmst**2,axis=1))
+vyrms = np.sqrt(np.average(vyrmst**2,axis=1))
+vzrms = np.sqrt(np.average(vzrmst**2,axis=1))
+
+bxrms = np.sqrt(np.average(bxrmst**2,axis=1))
+byrms = np.sqrt(np.average(byrmst**2,axis=1))
+bzrms = np.sqrt(np.average(bzrmst**2,axis=1))
+
+rorms = np.sqrt(np.average(rormst**2,axis=1))
+serms = np.sqrt(np.average(sermst**2,axis=1))
+prrms = np.sqrt(np.average(prrmst**2,axis=1))
+terms = np.sqrt(np.average(termst**2,axis=1))
+
+rom = np.average(romt,axis=1)
+sem = np.average(semt,axis=1)
+prm = np.average(prmt,axis=1)
+tem = np.average(temt,axis=1)
+
+fe = np.average(fet,axis=1)
+fd = np.average(fdt,axis=1)
+
+ff = fd*sr + fe*(1.e0-sr)
+
+fk = np.average(fkt,axis=1)
+fr = np.average(frt,axis=1)
+ft = ff + fk + fr
+
+np.savez(dir+"est.npz"\
+             ,x=x,y=y,z=z,rsun=rsun\
+             ,ro0=ro0,pr0=pr0,te0=te0,se0=se0\
+             ,vxrms=vxrms,vyrms=vyrms,vzrms=vzrms\
+             ,bxrms=bxrms,byrms=byrms,bzrms=bzrms\
+             ,rorms=rorms,prrms=prrms,serms=serms,terms=terms\
+             ,rom=rom,prm=prm,sem=sem,tem=tem\
+             ,ff=ff,fk=fk,fr=fr,ft=ft\
+             )
+         
+plt.rcParams["font.size"] = 15
+fig2 = plt.figure(num=2,figsize=(12,5))
+ax3 = fig2.add_subplot(121)
+ax4 = fig2.add_subplot(122)
+ax3.plot(xr,ff/fsun,color="red",label="$F_\mathrm{e}$")
+ax3.plot(xr,fk/fsun,color="green",label="$F_\mathrm{k}$")
+ax3.plot(xr,fr/fsun,color="blue",label="$F_\mathrm{r}$")
+ax3.plot(xr,ft/fsun,color="black",label="$F_\mathrm{t}$")
+ax3.set_xlim(xmin/rsun,xmax/rsun)
+ax3.set_ylim(fmin,fmax)
+ax3.set_xlabel("$x/R_{\odot}$")
+ax3.set_ylabel("$F/F_{\odot}$")
+ax3.set_title("Full convection zone")
+ax3.legend(loc='upper left',prop={'size': 15})
+ax3.annotate(s="t="+"{:.2f}".format(t[0]/3600./24.)+" [day]"\
+                 ,xy=[0.01,0.01],xycoords="figure fraction",fontsize=18)
+
+ax4.plot(xn,ff/fsun,color="red")
+ax4.plot(xn,fk/fsun,color="green")
+ax4.plot(xn,fr/fsun,color="blue")
+ax4.plot(xn,ft/fsun,color="black")
+ax4.set_xlim(-20,1)
+ax4.set_ylim(fmin,fmax)
+ax4.set_xlabel("$x - R_{\odot} \ [\mathrm{Mm}]$")
+ax4.set_ylabel("$F/F_{\odot}$")
+ax4.set_title("Around photosphere")
+fig2.tight_layout()
+plt.pause(0.001)
+plt.ion()
+    
---
+++ mov.py
@@ -0,0 +1,152 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import read
+import config as c
+import sys
+import os
+
+try:
+    caseid
+except NameError:
+    print("input caseid id (3 digit)")
+    caseid = 0
+    caseid = input()
+    caseid = "d"+caseid.zfill(3)
+
+dir="../run/"+caseid+"/data/"
+figdir="../figs/"
+casedir="../figs/"+caseid
+pngdir="../figs/"+caseid+"/png/"
+if not os.path.exists(figdir):
+    os.mkdir(figdir)
+if not os.path.exists(casedir):
+    os.mkdir(casedir)
+if not os.path.exists(pngdir):
+    os.mkdir(pngdir)
+
+read.read_init(dir,"3d")
+for key in c.p:
+    exec('%s = %s%s%s' % (key, 'c.p["',key,'"]'))
+
+try:
+    n0
+except NameError:
+    n0 = 0
+if  n0 > c.p["nd"]:
+    n0 = c.p["nd"]
+
+print("Maximum time step= ",nd," time ="\
+      ,dtout*float(nd)/3600./24.," [day]")
+
+plt.close('all')
+
+xsize = 10
+ysize = 10
+fig = plt.figure(num=1,figsize=(xsize,ysize))
+
+# read time
+f = open(dir+"time/t.dac."+'{0:08d}'.format(0),"rb")
+t0 = np.fromfile(f,endian+'d',1)
+f.close()    
+t0 = np.reshape(t0,(1),order="F")
+
+plt.rcParams["font.size"] = 15
+
+#n0 = 30
+#nd = n0
+    
+#for n in range(40,41):
+
+#jc = jx//2
+#kc = kx//2
+for n in range(n0,nd+1):
+#for n in range(0,1):
+#for n in range(n0,200):
+    print(n)
+    ##############################
+    # read time
+    f = open(dir+"time/t.dac."+'{0:08d}'.format(n),"rb")
+    t = np.fromfile(f,endian+'d',1)
+    f.close()    
+    t = np.reshape(t,(1),order="F")
+
+    ##############################
+    # read time
+    qq_in = read.read_tau_one(dir,n*int(ifac))
+
+    ##############################
+    # read value
+    f = open(dir+"remap/vla.dac."+'{0:08d}'.format(n),"rb")
+    vl0 = np.fromfile(f,c.p["endian"]+'f',m2da*ix*jx)
+    f.close()
+
+    vl = np.reshape(vl0,(ix,jx,m2da),order="F")
+
+    vc = {"a":0}
+    for m in range(c.p["m2da"]):
+        vc[c.p["cl"][m]] = vl[:,:,m]
+    ##############################
+
+    shading = "flat"
+    shading = "groroud"
+    
+    ax1 = fig.add_subplot(221,aspect="equal")
+    ax2 = fig.add_subplot(222,aspect="equal")
+    ax3 = fig.add_subplot(223,aspect="equal")
+    ax4 = fig.add_subplot(224,aspect="equal")
+
+    ax1.tick_params(labelbottom=False)#,bottom="off")
+    #ax1.tick_params(labelleft="off",left="off")
+    in0 = qq_in["in"].copy()
+    in0s = np.roll(in0,[jx//2-jc,kx//2-kc],axis=[0,1])
+    ax1.pcolormesh(y/rsun,z/rsun,in0s.transpose(),cmap='gist_gray',vmax=3.2e10,vmin=1.e10,shading=shading)
+    ax1.set_ylabel("z [$R_\odot$]")
+    ax1.set_title("Emergent intensity")
+
+    bx = np.roll(qq_in["bx"],[jx//2-jc,kx//2-kc],axis=[0,1])
+    by = np.roll(qq_in["by"],[jx//2-jc,kx//2-kc],axis=[0,1])
+    bz = np.roll(qq_in["bz"],[jx//2-jc,kx//2-kc],axis=[0,1])
+    #bx = qq_in[8,:,:]
+    ax2.tick_params(labelbottom=False)#,bottom="off")
+    ax2.tick_params(labelleft=False)#,left="off")
+    ax2.pcolormesh(y/rsun,z/rsun,bx.transpose(),cmap='gist_gray',vmax=2.5e3,vmin=-2.5e3,shading=shading)
+    ax2.set_title(r"LOS magnetic field@$\tau=1$")
+
+    #ax3.tick_params(labelbottom="off",bottom="off")
+    #ax3.tick_params(labelleft="off",left="off")
+
+    ses = np.roll((vc["sep"]-vc["sem"])/vc["serms"],jx//2-jc,axis=1)
+    tus = np.roll(vc["tup"],[jx//2-jc],axis=1)
+    ax3.pcolormesh(y/rsun,x/rsun,ses,vmax=3.,vmin=-3.,cmap='gist_heat',shading=shading)
+    ax3.contour(y/rsun,x/rsun,tus,levels=[1.],colors="w")
+    ax3.set_ylim((xmax-(ymax-ymin))/rsun,xmax/rsun)
+    #ax3.pcolormesh((vc["sep"]-vc["sem"])/vc["serms"],vmax=3.,vmin=-3.,cmap='gist_heat',shading=shading)
+    ax3.set_xlabel("y [$R_\odot$]")
+    ax3.set_title(r"$(s-\langle s\rangle)/s_{rms}$")
+    bb = np.sqrt(vc["bxm"]**2 + vc["bym"]**2 + vc["bzm"]**2)
+
+    bbs = np.roll(bb,[jx//2-jc],axis=1)
+    #ax4.tick_params(labelbottom="off",bottom="off")
+    ax4.tick_params(labelleft=False)#,left=False)
+    ax4.pcolormesh(y/rsun,x/rsun,bbs,vmax=1.e3,vmin=0.,cmap='gist_heat',shading=shading)
+    ax4.contour(y/rsun,x/rsun,tus,levels=[1.],colors="w")
+    ax4.set_ylim((xmax-(ymax-ymin))/rsun,xmax/rsun)
+    ax4.set_xlabel("y [$R_\odot$]")
+    ax4.set_ylabel("x [$R_\odot$]")
+    ax4.set_title(r"$\langle B^2\rangle$")
+    #ax4.pcolormesh(bb,vmax=1.e4,vmin=0.,cmap='gist_heat',shading=shading)
+
+    bbox_props = dict(boxstyle="round,pad=0.3", fc="white", ec="black", lw=2,alpha=0.9)
+    ax3.annotate(s="t="+"{:.2f}".format((t[0]-t0[0])/3600.)+" [hr]"\
+                     ,xy=[0.02,0.02],xycoords="figure fraction"\
+                     ,fontsize=18,color='black',bbox=bbox_props)
+
+    if(n == n0):
+        plt.tight_layout(pad=0.1)
+
+    plt.pause(0.1)
+    plt.savefig(pngdir+"py"+'{0:08d}'.format(n)+".png")
+
+    if(n != nd):
+        clf()
