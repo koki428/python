28093d53af238cd49b2f0ce06403fa4070ebc94f
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2020-01-01 23:52:44 +0900

    some information output from init.py

--- init.py
+++ init.py
@@ -28,3 +28,17 @@ except NameError:
 if  n0 > R2D2.p["nd"]:
     n0 = R2D2.p["nd"]
 
+print('### calculation domain ###')
+print('xmax - rsun = ', '{:6.2f}'.format((xmax - rsun)*1.e-8),'[Mm], xmin - rsun = ', '{:.2f}'.format((xmin - rsun)*1.e-8),'[Mm]')
+print('ymax        = ', '{:6.2f}'.format(ymax*1.e-8)       ,'[Mm], ymin        = ', '{:.2f}'.format(ymin*1.e-8),'[Mm]' )
+print('zmax        = ', '{:6.2f}'.format(zmax*1.e-8)       ,'[Mm], zmin        = ', '{:.2f}'.format(zmin*1.e-8),'[Mm]' )
+
+print('')
+print('### number of grid ###')
+print('(ix,jx,kx)=(',ix,',',jx,',',kx,')')
+
+print('')
+print('### calculation time ###')
+print('time step (nd) =',nd)
+t = R2D2.read_time(nd)
+print('time =','{:.2f}'.format(t/3600),' [hour]')
