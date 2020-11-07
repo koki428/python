1bec8d1c92c3d0d984cd24ce10d44f02061dd94e
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2020-11-07 17:01:18 +0900

    meanv changed

--- meanv.py
+++ meanv.py
@@ -29,12 +29,30 @@ if  n0 > d.p["nd"]:
     n0 = d.p["nd"]
 
 n0 = 0
+t = np.zeros(nd-n0+1)
 ekm = np.zeros(nd-n0+1)
-
-RR, TH = np.meshgrid(x,y,indexing='ij')
+bxmt = np.zeros((ix,jx,nd-n0+1))
+bymt = np.zeros((ix,jx,nd-n0+1))
+bzmt = np.zeros((ix,jx,nd-n0+1))
 
 for n in range(n0,nd+1):
     print(n)
 
+    d.read_time(n)
+    t[n-n0] = d.t
+
     d.read_vc(n,silent=True)
-    ekm[n-n0] = (RR**2*sin(TH)*d.vc['vzm']**2).mean()/(RR*2*sin(TH)).mean()
+    #ekm[n-n0] = (RR**2*sin(TH)*d.vc['vzm']**2).mean()/(RR*2*sin(TH)).mean()
+    bxmt[:,:,n-n0] = d.vc['bxm']
+    bymt[:,:,n-n0] = d.vc['bym']
+    bzmt[:,:,n-n0] = d.vc['bzm']
+
+plt.close('all')
+plt.clf()
+plt.figure(100,figsize=(8,4))
+plt.pcolormesh(t/86400/365,90-y/np.pi*180,bzmt[0,:,:],vmin=-8.e3,vmax=8.e3,cmap='bwr',shading='auto',rasterized=True)
+plt.xlabel('t [year]')
+plt.ylabel('latitude [degree]')
+plt.xlim(0,30)
+plt.tight_layout()
+plt.savefig(caseid+'.pdf')
